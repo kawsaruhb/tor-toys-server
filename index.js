fe09a552034914df96903403003c7e834c5e89d8
kawsaruhb <kawsaruhb@gmail.com>
2023-06-12 01:19:46 +0600

    delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,10 +34,17 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/addToy', async(req, res) => {
+        app.post('/addToy', async (req, res) => {
             const newToy = req.body;
             console.log(newToy);
             const result = await toyCollection.insertOne(newToy);
+            res.send(result);
+        })
+
+        app.delete('/addToy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query);
             res.send(result);
         })
 
